5391c6126e8031c9b01d2ee16f209ed5cfe7a026
chamrouensann <chamrouensann.geek@gmail.com>
2020-08-31 18:45:29 +0700

    Completed the project

--- daily_exercises_flutter_app/lib/screens/details_screen.dart
+++ daily_exercises_flutter_app/lib/screens/details_screen.dart
@@ -1,13 +1,16 @@
 import 'package:dailyexercisesflutterapp/constants.dart';
+import 'package:dailyexercisesflutterapp/widgets/bottom_nav_bar.dart';
 import 'package:dailyexercisesflutterapp/widgets/search_bar.dart';
 import 'package:dailyexercisesflutterapp/widgets/session_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
       body: Stack(
         children: <Widget>[
           Container(
@@ -23,84 +26,136 @@ class DetailsScreen extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: size.height * 0.05,
-                  ),
-                  Text(
-                    "Meditation",
-                    style: Theme.of(context)
-                        .textTheme
-                        .display1
-                        .copyWith(fontWeight: FontWeight.w900),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "3-10 MIN course",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: size.width * .6,
-                    child: Text(
-                        "Live happy and learn to live with other people and enjoy your life and make your dream happen. You should meditation 10mins a day to get good health."),
-                  ),
-                  SizedBox(
-                    child: SearchBar(),
-                    width: size.width * .4,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Wrap(
-                    spacing: 20,
-                    runSpacing: 20,
-                    children: <Widget>[
-                      SessionCard(
-                        sessionNum: 1,
-                        isDone: true,
-                        press: () {},
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    Text(
+                      "Meditation",
+                      style: Theme.of(context)
+                          .textTheme
+                          .display1
+                          .copyWith(fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "3-10 MIN course",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: size.width * .6,
+                      child: Text(
+                          "Live happy and learn to live with other people and enjoy your life and make your dream happen. You should meditation 10mins a day to get good health."),
+                    ),
+                    SizedBox(
+                      child: SearchBar(),
+                      width: size.width * .4,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      children: <Widget>[
+                        SessionCard(
+                          sessionNum: 1,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 2,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 3,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 4,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 5,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 6,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 7,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 8,
+                          isDone: true,
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Meditation",
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
                       ),
-                      SessionCard(
-                        sessionNum: 2,
-                        isDone: true,
-                        press: () {},
+                      child: Row(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            "assets/icons/Meditation_women_small.svg",
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Basic 2",
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text("Start your deepen on the practices")
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: SvgPicture.asset("assets/icons/Lock.svg"),
+                          )
+                        ],
                       ),
-                      SessionCard(
-                        sessionNum: 3,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 4,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 5,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 6,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 7,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 8,
-                        isDone: true,
-                        press: () {},
-                      ),
-                    ],
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           )
